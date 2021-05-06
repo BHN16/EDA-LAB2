b004bc77a7253c87cae617b05db377f038dd41f7
L4mb0 <juan.navarro@utec.edu.pe>
2021-05-06 02:13:35 -0500

    unit test

--- Problema2/main.cpp
+++ Problema2/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <thread>
 #include "safestack.h"
+#include <assert.h>
+
 using namespace std;
+
 
 void func (SafeStack<char>*s, int n, char c) {
     for (int i = 0; i < n; i++) {
@@ -9,40 +12,47 @@ void func (SafeStack<char>*s, int n, char c) {
     }
 }
 
+
+class tester{
+public:
+    void test(int a, int b, int c, int d, int e){
+	thread threads[5];
+	SafeStack<char> s;
+    	SafeStack<char>* s1 = new SafeStack<char>(); 
+    
+	threads[0] = thread(func, s1, a, 'A');
+    	threads[1] = thread(func, s1, b, 'B');
+    	threads[2] = thread(func, s1, c, 'C');
+    	threads[3] = thread(func, s1, d, 'D');
+    	threads[4] = thread(func, s1, e, 'E');
+  	for (int i = 0; i < 5; i++) {
+        	threads[i].join();
+    	}
+    	while (!s1->is_empty()) {
+        	cout << s1->peek() << endl;
+		switch(s1->peek()){
+			case 'A': a--;
+			case 'B': b--;
+			case 'C': c--;
+			case 'D': d--;
+			case 'E': e--;
+		}
+        	s1->pop();
+    	}
+
+	if(a==b==c==d==e==0) cout<<"Test passed\n";
+	else cout<<"Test failed\n";
+
+
+    }
+
+};
+
+
 int main () {
-    thread threads[5];
-    SafeStack<char> s;
-    SafeStack<char>* s1 = new SafeStack<char>(); 
-    for (int i = 0; i < 3; i++) {
-        s.push('A');
-    }
-    for (int i = 0; i < 6; i++) {
-        s.push('B');
-    }
-    for (int i = 0; i < 2; i++) {
-        s.push('C');
-    }
-    for (int i = 0; i < 5; i++) {
-        s.push('D');
-    }
-    for (int i = 0; i < 8; i++) {
-        s.push('E');
-    }
-    while (!s.is_empty()) {
-        cout << s.peek() << endl;
-        s.pop ();
-    }
-    threads[0] = thread(func, s1, 3, 'A');
-    threads[1] = thread(func, s1, 6, 'B');
-    threads[2] = thread(func, s1, 2, 'C');
-    threads[3] = thread(func, s1, 5, 'D');
-    threads[4] = thread(func, s1, 8, 'E');
-    for (int i = 0; i < 5; i++) {
-        threads[i].join();
-    }
-    while (!s1->is_empty()) {
-        cout << s1->peek() << endl;
-        s1->pop();
-    }
+    
+    tester t;
+    t.test(1,2,3,4,5);
+
     return 0;
 }
